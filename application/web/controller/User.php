9feba5684df4928e0da1b2956d8767b061bca77f
licai <php_programmer@163.com>
2018-02-28 17:42:09 +0800

    aaa

--- application/web/controller/User.php
+++ application/web/controller/User.php
@@ -2,11 +2,16 @@
 namespace app\web\controller;
 
 use app\web\controller\Yang;
+use app\common\model\User as U;
+use think\Session;
 
 class User extends Yang
 {
     public function index()
     {
+        $id = session('user.id');
+        $user = U::where('id',$id)->find();
+        $this->assign('user',$user);
         return $this->fetch();
     }
     public function edit()
