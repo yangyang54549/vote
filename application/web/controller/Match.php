e07b8c360963e32239bfa49742ddaabb3210d739
licai <php_programmer@163.com>
2018-02-28 13:41:18 +0800

    sss

--- application/web/controller/Match.php
+++ application/web/controller/Match.php
@@ -2,11 +2,15 @@
 namespace app\web\controller;
 
 use app\web\controller\Yang;
+use app\common\model\Type;
 
 class Match extends Yang
 {
     public function index()
     {
+        $type = Type::select();
+        $this->assign('type',$type);
+
         return $this->fetch();
     }
     public function info()
