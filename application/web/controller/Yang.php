3ce6419a9f96246618459f26df55c114b73f3ec0
licai <php_programmer@163.com>
2018-03-08 14:39:21 +0800

    aaaaaaaaaa

--- application/web/controller/Yang.php
+++ application/web/controller/Yang.php
@@ -21,7 +21,20 @@ class Yang extends Controller
         if (!in_array($url,$this->arr)) {
             $suser = session('user');
             if (!isset($suser)) {
-                $this->redirect('web/login/login');
+                //未登录
+                if($url == 'User/tui'){
+                    $invite = input('invite');
+                    //cookie('referrer',$invite,2592000);
+                    $this->redirect('login/reg', ['referrer' => $invite]);
+                }
+                $this->redirect('login/login');
+            }
+            //已登录又点击链接跳转至首页
+            if($url == 'User/tui'){
+                $invite = input('invite');
+                if ($invite == session('user.referrer')) {
+                    $this->redirect('index/index');
+                }
             }
             $this->id = session('user.id');
         }
