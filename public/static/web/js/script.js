1f0df8fd9f6e8a0786e5ab7b4b413c102de8fcc2
赵 <13203940281@163.com>
2018-03-10 15:24:58 +0800

    12313213

---
+++ public/static/web/js/script.js
@@ -0,0 +1,16 @@
+function toasts(){					
+				setTimeout(function() {
+					var ss = document.getElementsByClassName("mui-toast-container");
+					if(ss.length>0)					
+				ss.style.display = "none";
+				}, 5000);				
+			}
+//			$(document).bind("contextmenu copy selectstart", function(){
+//				return false;
+//			});
+//			/*禁用ctrl+c ctrl+v*/
+//			$(document).keydown(function(e){
+//				if(e.ctrlKey &&(e.keyCode ==65|| e.keyCode ==67)){
+//					return false;
+//				}
+//			})
